4cfd6349bf3ba54b50786e1ceca5db6116933c8d
ravikiran-1 <17pa1a04g1@vishnu.edu.in>
2020-07-23 20:37:37 +0530

    Updates

---
+++ precache-manifest.41fd06da6b9f373914eff982d9bd2951.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed4fa7de7a276e89b5b2474f014da27",
+    "url": "/weather_delay/index.html"
+  },
+  {
+    "revision": "2b37b1dc08c204e80e3a",
+    "url": "/weather_delay/static/css/main.d4d01906.chunk.css"
+  },
+  {
+    "revision": "4a79708fe2bb8d98f2f2",
+    "url": "/weather_delay/static/js/2.07144284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_delay/static/js/2.07144284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b37b1dc08c204e80e3a",
+    "url": "/weather_delay/static/js/main.abe64ad3.chunk.js"
+  },
+  {
+    "revision": "0bf8f8eb6bcd4d4a7717",
+    "url": "/weather_delay/static/js/runtime-main.63fe1b17.js"
+  },
+  {
+    "revision": "62eb1a8da5363fc744b759d0eb816a65",
+    "url": "/weather_delay/static/media/cold.62eb1a8d.jpg"
+  },
+  {
+    "revision": "9eb6fceea2de725085897e7dd7a81b6a",
+    "url": "/weather_delay/static/media/warm.9eb6fcee.jpg"
+  }
+]);
